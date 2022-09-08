52a0dc4c53eeeecfa7f473cf53816e3130f0d39d
Whitney Muchira <whitneymuchira@gmail.com>
2022-09-08 22:35:31 +0300

    theme setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+import App from './App'
+import './css/index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <React.Fragment>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
-);
+  </React.Fragment>,
+  document.getElementById('root')
+)
+Footer
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
